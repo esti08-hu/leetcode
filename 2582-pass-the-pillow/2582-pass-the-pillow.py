0178b7d8eb6313d1c98515ddceb3e8e7213d68a5
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-01-31 19:44:51 +0300

    Time: 0 ms (100%), Space: 17.8 MB (20.16%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -1,13 +1,27 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
-        num = [i+1 for i in range(n)]
-        if n > time:
-            return num[time]
-        num_1 = time-n
-        num_2 = num_1//(n-1)
-        rem = num_1%(n-1)
+        # num = [i+1 for i in range(n)]
+        # if n > time:
+        #     return num[time]
+        # num_1 = time-n
+        # num_2 = num_1//(n-1)
+        # rem = num_1%(n-1)
         
-        if num_2%2==0 or num_2==0:
-            return (num[-rem-2])
-        else:
-            return num[rem+1]
+        # if num_2%2==0 or num_2==0:
+        #     return (num[-rem-2])
+        # else:
+        #     return num[rem+1]
+
+        num = 1
+        for i in range(time):
+            if num == 1:
+                num+=1
+                inc = True
+            elif num==n:
+                num-=1
+                inc = False
+            elif num > 1 and inc == True:
+                num+=1
+            elif num < n and inc == False:
+                num-=1
+        return num
