f57ce9c303eb8feb53d3ebd7c842d8726b72f41f
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-02-12 15:20:41 +0300

    Time: 8 ms (32.4%), Space: 18.7 MB (74.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        p_holer = 0
-
-        for seeker in range(len(nums)):
+        p_holder = 0
+        seeker = 0
+        # for seeker in range(len(nums)):
+        while seeker < len(nums):
             if nums[seeker] != 0:
-                nums[p_holer], nums[seeker] = nums[seeker], nums[p_holer]
-                p_holer += 1
+                nums[p_holder], nums[seeker] = nums[seeker], nums[p_holder]
+                p_holder += 1
+            
+            seeker += 1
