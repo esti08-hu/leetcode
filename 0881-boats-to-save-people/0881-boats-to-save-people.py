250e7d22b27a72e32448c9962b81a29f84e7a714
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-02-12 18:02:06 +0300

    Time: 55 ms (15.09%), Space: 23.4 MB (21.59%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -4,9 +4,9 @@ class Solution:
         left = 0
         right = len(people)-1
         count = 0
-        
+
         while left <= right:
-            if people[left] + people[right] <= limit:
+            if sum([people[left], people[right]]) <= limit:
                 left+=1
             right-=1
             count+=1
