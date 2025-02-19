96b53d4a3fc5a281b983b27e6c4729dc930f8625
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-02-19 13:30:50 +0300

    Time: 99 ms (36.79%), Space: 44.5 MB (93.26%) - LeetHub

--- 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
@@ -1,19 +1,22 @@
 class Solution:
     def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
         prefix_sum = [0]*(len(nums)+1)
+
         for req in requests:
             l,r = req
 
             prefix_sum[l] += 1
-            prefix_sum[r+1] -= 1
+            if r+1 < len(prefix_sum):
+                prefix_sum[r+1] -= 1
 
         for i in range(1, len(prefix_sum)):
-            prefix_sum[i] = prefix_sum[i] + prefix_sum[i-1]
+            prefix_sum[i] += prefix_sum[i-1]
         
-        prefix_sum.sort(reverse=True)
+        prefix_sum.pop()
 
+        prefix_sum.sort(reverse=True)
         nums.sort(reverse=True)
 
-        max_sum = sum([prefix_sum[i]*nums[i] for i in range(len(nums))])
+        max_sum = sum(prefix_sum[i]*nums[i] for i in range(len(nums)))
 
-        return max_sum%(10**9 + 1)
+        return max_sum%(10**9 + 7)
