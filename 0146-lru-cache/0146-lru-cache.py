d9536444066878ea9f01e301235998f495874429
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-02-24 20:50:47 +0300

    Time: 197 ms (11.73%), Space: 78.6 MB (27.5%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {} 
+        self.size = 0
+
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def _add_to_tail(self, node):
+        prev, nxt = self.tail.prev, self.tail
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+        self._remove(node)
+        self._add_to_tail(node)
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._remove(node)
+            self._add_to_tail(node)
+        else:
+            if self.size == self.capacity:
+                lru_node = self.head.next
+                self._remove(lru_node)
+                del self.cache[lru_node.key]
+                self.size -= 1
+
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self._add_to_tail(new_node)
+            self.size += 1
