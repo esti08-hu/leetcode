e9bb627b4738f9dc999795de94532fb9658adc2a
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-03-06 18:15:54 +0300

    Time: 0 ms (100%), Space: 17.9 MB (65.36%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,15 +1,21 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stk = []
+        dict_s = {}
         res = []
-        for n1 in nums1:
-            idx = nums2.index(n1)
-            found  = False 
-            for i in range(idx+1, len(nums2)): 
-                if nums2[i] > n1:
-                    found = True
-                    res.append(nums2[i])
-                    break
-            if not found:
+        for n2 in nums2:
+            if not stk:
+                stk.append(n2)
+            else:
+                while stk and stk[-1] < n2:
+                    x = stk.pop()
+                    dict_s[x] = n2
+                stk.append(n2)
+
+        for n in nums1:
+            if n not in dict_s:
                 res.append(-1)
+            else:
+                res.append(dict_s[n])
         
         return res
