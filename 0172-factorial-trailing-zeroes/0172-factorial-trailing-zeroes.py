3b56eef64f2f32466d2f68b14557b8b2d06b6230
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-03-11 18:35:15 +0300

    Time: 2340 ms (15.54%), Space: 18.2 MB (5.24%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        sys.set_int_max_str_digits(1000000)
+
+        n = (math.factorial(n))
+        n = str(n)
+        count = 0
+        if n[-1]=="0":
+            for i in range(len(n)-1, -1, -1):
+                if n[i] == "0":
+                    count+=1
+                else:
+                    break
+        return count
