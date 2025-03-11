aed123d4236fc1ce774af795c57100b9ee82d7dc
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-03-11 18:44:36 +0300

    Time: 4013 ms (13.99%), Space: 19.1 MB (5.24%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,10 +1,16 @@
 class Solution:
+        
+    def factorial_recursive(self, n):
+        if n == 0:
+            return 1
+        return n * self.factorial_recursive(n - 1)
+
     def trailingZeroes(self, n: int) -> int:
         sys.set_int_max_str_digits(1000000)
 
-        n = str(math.factorial(n))
-
+        n = str(self.factorial_recursive(n))
         count = 0
+
         if n[-1]=="0":
             for i in range(len(n)-1, -1, -1):
                 if n[i] == "0":
