c5870a30a3c28a37d16eb6a46773a81048ee2de0
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-03-29 22:39:25 +0300

    Time: 0 ms (100%), Space: 18.1 MB (29.1%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        ans_count = Counter(answers)
+
+        total = 0
+        for a, c in ans_count.items():
+            size = a + 1
+            groups = (c + size - 1) // size
+            total += (groups * size)
+        
+        return total
