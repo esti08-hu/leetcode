775fd523926eeda81ce817809e508d92d5859ea8
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-04-09 16:22:43 +0300

    Time: 22 ms (91.71%), Space: 22.8 MB (34.19%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,11 +1,11 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        graph = [[] for _ in range(n)]
-        
+        graph = [[] for _ in range(n+1)]
+
         for l, r in dislikes:
-            graph[l-1].append(r-1)
-            graph[r-1].append(l-1)
-            
+            graph[l].append(r)
+            graph[r].append(l)
+
         def dfs(node):
             for neighbour in graph[node]:
                 if neighbour in colors:
@@ -16,18 +16,16 @@ class Solution:
                     colors[neighbour] = 1 - colors[node]
                     if not dfs(neighbour):
                         return False
-                        
+
             return True 
 
         colors = {}
 
-        for node in range(len(graph)):
-            if graph[node] and node not in colors:
+        for node in range(1, n+1):
+            if node not in colors:
                 colors[node] = 0
 
                 if not dfs(node):
                     return False  
 
         return True
-
-        
