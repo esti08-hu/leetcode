35e76787c2c87d483c9334fddfa1e68541d4fa0b
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-04-11 23:10:25 +0300

    Time: 431 ms (35.38%), Space: 26.8 MB (25.8%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -3,10 +3,10 @@ class Solution:
         def invert(s):
             return ''.join('1' if char == '0' else '0' for char in s)
 
-        def generate_sn(n):
+        def generate(n):
             if n == 1:
                 return "0"
             
-            prev = generate_sn(n - 1)
+            prev = generate(n - 1)
             return prev + "1" + invert(prev)[::-1] 
-        return generate_sn(n)[k-1]
+        return generate(n)[k-1]
