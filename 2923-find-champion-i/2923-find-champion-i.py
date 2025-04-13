a8cd24809978a404e42f4bb50366ff4a9fb84f35
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-04-13 18:09:42 +0300

    Time: 0 ms (100%), Space: 18.4 MB (78.56%) - LeetHub

--- 2923-find-champion-i/2923-find-champion-i.py
+++ 2923-find-champion-i/2923-find-champion-i.py
@@ -1,17 +1,9 @@
 class Solution:
     def findChampion(self, grid: List[List[int]]) -> int:
-        n = len(grid[0])
-        track = defaultdict(int)
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    track[i]+=1
-        
-        champ = [-1,-1]
+        n = len(grid)
 
-        for k,v in track.items():
-            if v > champ[1]:
-                champ[0] = k
-                champ[1] = v
+        for i in range(n):
+            if sum(grid[i]) == n -1:
+                return i
+
         
-        return champ[0]
