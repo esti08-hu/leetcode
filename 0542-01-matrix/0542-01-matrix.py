a809b26b675b2dc713aed4633ad5ca15ca43dce2
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-04-17 21:13:41 +0300

    Time: 107 ms (89.61%), Space: 19.5 MB (93.3%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,27 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        rows = len(mat)
+        cols = len(mat[0])
+        q = deque()
+        for r in range(rows):
+            for c in range(cols):
+                if mat[r][c] == 0:
+                    q.append((r,c))
+                else:
+                    mat[r][c] = -1
+                    
+        directions = [[0,1], [0,-1], [1,0], [-1,0]]
+        level = 1
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    row, col = dr + r, dc + c
+                    if (row < 0 or row == len(mat) or
+                        col < 0 or col == len(mat[0]) or
+                        mat[row][col] != -1):
+                        continue
+                    mat[row][col] = level
+                    q.append((row, col))
+            level+=1
+        return mat
