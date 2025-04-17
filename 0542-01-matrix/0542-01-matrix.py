5d058b9e275e99cc26e26b76fd4ea9b10a81fc5f
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-04-17 21:21:42 +0300

    Time: 102 ms (91.97%), Space: 19.5 MB (93.3%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -17,8 +17,8 @@ class Solution:
                 r, c = q.popleft()
                 for dr, dc in directions:
                     row, col = dr + r, dc + c
-                    if (row < 0 or row == len(mat) or
-                        col < 0 or col == len(mat[0]) or
+                    if (row < 0 or row == rows or
+                        col < 0 or col == cols or
                         mat[row][col] != -1):
                         continue
                     mat[row][col] = level
