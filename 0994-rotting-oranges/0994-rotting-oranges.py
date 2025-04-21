dea29b39e3ec944852669ec8756d8241d4c96239
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-04-21 13:00:55 +0300

    Time: 7 ms (27.19%), Space: 17.9 MB (45.85%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,39 +1,45 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-
-        rows = len(grid)
+        rows = len(grid) 
         cols = len(grid[0])
+
+        def inBound(r,c):
+            return(0<=r<rows and 0<=c<cols)
 
         fresh = 0
         q = deque()
+
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 2:
                     q.append((i,j))
                 elif grid[i][j] == 1:
-                    fresh +=1
-
+                    fresh += 1
+        
+        directions = [(0,1), (1,0), (0,-1), (-1,0)]
         time = 0
-        directions = [(0,1), (1,0), (-1,0), (0,-1)]
-
-        def inBound(r,c):
-            return (0<=r<rows and 0<=c<cols)
-
         while q and fresh > 0:
-            for i in range(len(q)):
-                r,c = q.popleft()
+            len_q =  len(q)
+
+            for _ in range(len_q):
+                r, c = q.popleft()
 
                 for dr, dc in directions:
-                    nr, nc = r+dr, c+dc
+                    nr,nc = r+dr, c+dc
 
-                    if inBound(nr,nc):
-                        if grid[nr][nc]==1:
-                            grid[nr][nc] = 2
+                    if inBound(nr, nc):
+                        if grid[nr][nc] == 1:
+                            grid[nr][nc]=2
+                            fresh-=1
                             q.append((nr, nc))
-                            fresh -= 1
-            time += 1
+            time +=1
         
         return time if fresh==0 else -1
+
+
+
+
+
 
 
 
