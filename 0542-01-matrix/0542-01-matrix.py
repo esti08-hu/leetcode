39c8cc1c4887a74c9e456474ad2128328eff7595
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-04-21 23:27:59 +0300

    Time: 152 ms (54.11%), Space: 19.6 MB (84.18%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,6 +2,10 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         rows = len(mat)
         cols = len(mat[0])
+        
+        def isBound(r,c):
+            return (0 <= r < rows and 0 <= c < cols)
+
         q = deque()
         for r in range(rows):
             for c in range(cols):
@@ -9,7 +13,7 @@ class Solution:
                     q.append((r,c))
                 else:
                     mat[r][c] = -1
-                    
+
         directions = [[0,1], [0,-1], [1,0], [-1,0]]
         level = 1
         while q:
@@ -17,11 +21,12 @@ class Solution:
                 r, c = q.popleft()
                 for dr, dc in directions:
                     row, col = dr + r, dc + c
-                    if (row < 0 or row == rows or
-                        col < 0 or col == cols or
-                        mat[row][col] != -1):
-                        continue
-                    mat[row][col] = level
-                    q.append((row, col))
+                    if isBound(row, col):
+                        if mat[row][col]==-1:
+
+                            mat[row][col] = level
+                            q.append((row, col))
+
             level+=1
+
         return mat
