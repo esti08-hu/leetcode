1daba1646c9c5b4186e54d9a986d5a88f57b3c3d
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-04-22 20:49:54 +0300

    Time: 863 ms (60.53%), Space: 84.8 MB (69.41%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.py
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.py
@@ -2,29 +2,27 @@ class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
         rows = len(isWater)
         cols = len(isWater[0])
-        q = deque()
-        for r in range(rows):
-            for c in range(cols):
-                if isWater[r][c] == 1:
-                    q.append((r,c))
-                    isWater[r][c] = 0
-                else:
-                    isWater[r][c] = -1
-                    
-        directions = [[0,1], [0,-1], [1,0], [-1,0]]
-        level = 1
-        while q:
-            for i in range(len(q)):
-                r, c = q.popleft()
-                for dr, dc in directions:
-                    row, col = dr + r, dc + c
-                    if (row < 0 or row == rows or
-                        col < 0 or col == cols or
-                        isWater[row][col] != -1):
-                        continue
-                    isWater[row][col] = level
-                    q.append((row, col))
-            level+=1
-        return isWater
-        
 
+        def inBound(r, c):
+            return 0 <= r < rows and 0 <= c < cols
+
+        q = deque()
+
+        height = [[-1] * cols for _ in range(rows)]
+        for i in range(rows):
+            for j in range(cols):
+                if isWater[i][j] == 1:
+                    q.append((i, j))
+                    height[i][j] = 0  
+                    
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        while q:
+            r, c = q.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if inBound(nr, nc) and height[nr][nc] == -1:
+                    height[nr][nc] = height[r][c] + 1
+                    q.append((nr, nc))
+
+        return height
