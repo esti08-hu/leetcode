1350eff8a02c923dda9371333cd8c723af5570e2
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-05-03 21:24:58 +0300

    Time: 47 ms (81.74%), Space: 28.6 MB (71.99%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,14 +1,12 @@
-import heapq
-
 class Solution:
-    def furthestBuilding(self, heights, bricks, ladders):
-        pq = []  # Priority queue (min-heap) to store differences in heights
-        for i in range(1, len(heights)):
-            diff = heights[i] - heights[i-1]
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        heap = []
+        for i in range(len(heights) - 1):
+            diff = heights[i + 1] - heights[i]
             if diff > 0:
-                heapq.heappush(pq, diff)
-            if len(pq) > ladders:
-                bricks -= heapq.heappop(pq)
-            if bricks < 0:
-                return i - 1
+                heappush(heap, diff)
+                if len(heap) > ladders:
+                    bricks -= heappop(heap)
+                if bricks < 0:
+                    return i
         return len(heights) - 1
