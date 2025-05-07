92c599c334f199ffc07d28cfc4ed47eee52ed0dd
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-05-07 18:26:53 +0300

    Time: 288 ms (96.52%), Space: 57.7 MB (73.41%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        heap  = []
+        tasks = sorted([(start, duration, i) for i, (start, duration) in enumerate(tasks)])
+        n = len(tasks)
+        result = []
+        i = 0
+        time = 0
+        while i < n or heap:
+            if not heap:
+                time = max(time, tasks[i][0])
+            while i < n and tasks[i][0] <= time:
+                heappush(heap, (tasks[i][1], tasks[i][2]))
+                i += 1
+            duration, index = heappop(heap)
+            result.append(index)
+            time += duration
+        return result
+
+    
