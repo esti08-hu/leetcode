42b590c74393cc74c1e225db30e1c1d32059f49a
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-05-16 19:22:01 +0300

    Time: 2431 ms (18.21%), Space: 17.6 MB (91%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,21 +1,11 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        def helper(start, end, player1_turn):
-            if start > end:
-                return 0
-
-            if player1_turn:
-                pick_start = nums[start] + helper(start + 1, end, False)
-                pick_end = nums[end] + helper(start, end - 1, False)
-                return max(pick_start, pick_end)
-            else:
-                pick_start = helper(start + 1, end, True)
-                pick_end = helper(start, end - 1, True)
-                return min(pick_start, pick_end)
-
-        total_sum = sum(nums)
-
-        p1_score = helper(0, len(nums) - 1, True)
-        p2_score = total_sum - p1_score
-
-        return p1_score >= p2_score
+        def helper(nums, l, r):
+            if l == r:
+                return nums[l]
+            left = nums[l] - helper(nums, l + 1, r)
+            right = nums[r] - helper(nums, l, r - 1)
+            return max(left, right)
+        
+        return helper(nums, 0, len(nums) - 1) >= 0
+         
