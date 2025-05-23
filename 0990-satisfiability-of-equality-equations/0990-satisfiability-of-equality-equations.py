4a1b57ac6dfc4b895a59b6e80074c0319ca4441a
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-05-23 22:48:45 +0300

    Time: 1 ms (75.95%), Space: 18 MB (56.09%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -1,22 +1,21 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
         parent = [i for i in range(26)]
-        def find(x):
-            if x == parent[x]:
-                return x
-            parent[x] = find(parent[x])
-            return parent[x]
-        def union(x, y):
-            rx = find(x)
-            ry = find(y)
-            if rx != ry:
-                parent[rx] = ry
-        for eq in equations:
-            if eq[1] == "=":
-                union(ord(eq[0]) - ord("a"), ord(eq[3]) - ord("a"))
-        for eq in equations:
-            if eq[1] == "!":
-                if find(ord(eq[0]) - ord("a")) == find(ord(eq[3]) - ord("a")):
-                    return False
-        return True
 
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            parent[find(x)] = find(y)
+
+        for eq in equations:
+            if eq[1] == '=':
+                union(ord(eq[0]) - ord('a'), ord(eq[3]) - ord('a'))
+
+        for eq in equations:
+            if eq[1] == '!' and find(ord(eq[0]) - ord('a')) == find(ord(eq[3]) - ord('a')):
+                return False
+
+        return True
