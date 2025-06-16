4eea65c860b8fb22f0a5ff142f7f1776552c10ec
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-06-16 23:53:35 +0300

    Time: 1627 ms (43.18%), Space: 85.3 MB (54.33%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,16 +1,36 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        def find(parent, x):
+            if parent[x] != x:
+                parent[x] = find(parent, parent[x])
+            return parent[x]
+
+        def union(parent, rank, x, y):
+            rootX = find(parent, x)
+            rootY = find(parent, y)
+            if rootX != rootY:
+                if rank[rootX] > rank[rootY]:
+                    parent[rootY] = rootX
+                elif rank[rootX] < rank[rootY]:
+                    parent[rootX] = rootY
+                else:
+                    parent[rootY] = rootX
+                    rank[rootX] += 1
+
         n = len(points)
-        visited = [False] * n
-        heap = [(0, 0)]
-        cost = 0
-        while heap:
-            dist, u = heappop(heap)
-            if visited[u]:
-                continue
-            visited[u] = True
-            cost += dist
-            for v in range(n):
-                if not visited[v]:
-                    heappush(heap, (abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]), v))
-        return cost
+        edges = []
+        for i in range(n):
+            for j in range(i + 1, n):
+                cost = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                edges.append((cost, i, j))
+
+        edges.sort()
+        parent = list(range(n))
+        rank = [0] * n
+        total_cost = 0
+        for cost, u, v in edges:
+            if find(parent, u) != find(parent, v):
+                union(parent, rank, u, v)
+                total_cost += cost
+
+        return total_cost
