ae7c2f2576ecfb8b012e373fb66195476230d58d
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-06-22 06:46:39 +0300

    Time: 2265 ms (24.98%), Space: 17.6 MB (98.05%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,10 +1,7 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        def helper(nums, l, r):
-            if l == r:
-                return nums[l]
-            left = nums[l] - helper(nums, l + 1, r)
-            right = nums[r] - helper(nums, l, r - 1)
-            return max(left, right)
-        
-        return helper(nums, 0, len(nums) - 1) >= 0
+        def dfs(start, end):
+            if start == end:
+                return nums[start]
+            return max(nums[start] - dfs(start + 1, end), nums[end] - dfs(start, end - 1))
+        return dfs(0, len(nums) - 1) >= 0
