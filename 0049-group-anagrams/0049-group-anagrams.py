e5a8bee3761197d4af228a7665fcd9d4b132a415
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-06-26 21:02:09 +0300

    Time: 7 ms (98.09%), Space: 20.8 MB (56.46%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,11 @@
-from collections import Counter, defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        sorted_strs = []
-        strs_dict = defaultdict(list)
-
-        for s1 in strs:
-            sorted_strs.append("".join(sorted(s1)))
+        anagrams = {}
         
-        set_sorted_strs = set(sorted_strs)
-        for s2 in strs:
-            if "".join(sorted(s2)) in set_sorted_strs:
-                strs_dict["".join(sorted(s2))].append(s2)
+        for s in strs:
+            key = ''.join(sorted(s))
+            if key not in anagrams:
+                anagrams[key] = []
+            anagrams[key].append(s)
         
-        return list(strs_dict.values())
+        return list(anagrams.values())
