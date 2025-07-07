1f3d22ad86159323039e844e45d68869bb559520
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-07-07 22:01:46 +0300

    Time: 3 ms (50.3%), Space: 17.8 MB (65.61%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
@@ -1,7 +1,4 @@
-from collections import Counter
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        nums_count = Counter(nums)
-        sorted_num = (sorted(nums_count, key=nums_count.get))
-
-        return [sorted_num[-1], sorted_num[-2]]
+        count = Counter(nums)
+        return sorted([num for num in count if count[num] > 1])
