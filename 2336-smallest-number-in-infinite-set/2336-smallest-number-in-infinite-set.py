01b00a1bd0d1156e8d93666ddc7f7d12f21128c9
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-07-11 21:03:33 +0300

    Time: 4 ms (98.34%), Space: 18.7 MB (12.82%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
@@ -2,23 +2,22 @@ class SmallestInfiniteSet:
 
     def __init__(self):
         self.current = 1
-        self.added = set()
+        self.num_set = set()
         
 
     def popSmallest(self) -> int:
-        if self.added:
-            smallest = min(self.added)
-            self.added.remove(smallest)
+        if self.num_set:
+            smallest = min(self.num_set)
+            self.num_set.remove(smallest)
             return smallest
         else:
             smallest = self.current
             self.current += 1
             return smallest
-        
 
     def addBack(self, num: int) -> None:
-        if num < self.current and num not in self.added:
-            self.added.add(num)
+        if num < self.current and num not in self.num_set:
+            self.num_set.add(num)
 
 
 # Your SmallestInfiniteSet object will be instantiated and called as such:
