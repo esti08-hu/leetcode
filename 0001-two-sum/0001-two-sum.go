4c279e88c413b3856a3902d56a866adb605087e8
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-08-04 22:41:42 +0300

    Time: 1 ms (56.03%), Space: 6 MB (41.32%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+	for i, num := range nums {
+		if j, ok := numMap[target-num]; ok {
+			return []int{j, i}
+		}
+		numMap[num] = i
+	}
+	return nil
+}
