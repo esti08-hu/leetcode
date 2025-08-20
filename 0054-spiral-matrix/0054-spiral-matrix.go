955c4a1fc1a9f14d1eb5e3dad7c392db65df3955
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-08-20 20:39:41 +0300

    Time: 0 ms (100%), Space: 3.9 MB (84.6%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.go
@@ -0,0 +1,35 @@
+func spiralOrder(matrix [][]int) []int {
+    res := []int{}
+
+        t, b := 0, len(matrix) - 1
+        l, r := 0, len(matrix[0]) - 1
+
+        
+    for t <= b && l <= r{
+        for i:=l; i < r+1; i++ {
+            res = append(res, matrix[t][i])
+        }
+        t+=1
+
+        for i:=t; i < b+1; i++ {
+            res = append(res, matrix[i][r])
+        }
+        r-=1
+            
+        if t <= b {
+            for i:=r; i > l -1; i-- {
+                res = append(res, matrix[b][i])
+            }
+        }
+        b-=1
+
+        if l <= r {
+            for i:=b; i > t- 1; i-- {
+                res = append(res, matrix[i][l])
+            }
+        }
+        l += 1
+            
+    }
+    return res
+}
