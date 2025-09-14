c84571628aee3af1533de766dda26dd7713810d7
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-09-14 22:19:29 +0300

    Time: 0 ms (100%), Space: 17.8 MB (43.43%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,13 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        n1 = []
-        n2 = []
-
-        for i in range(m):
-            n1.append(nums1[i])
-        for j in range(n):
-            n2.append(nums2[j])
-
-        n1.extend(n2)
-        nums1[:] = sorted(n1)
+        i, j, k = m - 1, n - 1, m + n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
