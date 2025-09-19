c2371c3cde43f0277ed6fdf5b2689e6cd5307ca6
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-09-19 21:18:20 +0300

    Time: 35 ms (52.31%), Space: 19.3 MB (64.05%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,29 +1,30 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         rows, cols = len(heights), len(heights[0])
-
+        
         p_set = set()
         a_set = set()
-
+        
         def dfs(i, j, prev, visited):
-            if i<0 or j<0 or i>=rows or j>=cols or heights[i][j] < prev or (i, j) in visited:
-                return 
-
-            visited.add((i,j))
-
-            dfs(i+1, j, heights[i][j], visited)
-            dfs(i, j+1, heights[i][j], visited)
-            dfs(i-1, j, heights[i][j], visited)
-            dfs(i, j-1, heights[i][j], visited)
-
+            if i <0  or j<0  or i>=rows or j>=cols or (i, j) in visited or heights[i][j] < prev:
+                return
+            
+            visited.add((i, j))
+            
+            for di, dj in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                ni, nj = i+di, j+dj
+                
+                dfs(ni, nj, heights[i][j], visited)
+            
         for i in range(rows):
-            dfs(i,0,-1,p_set)
+            dfs(i, 0, -1, p_set)
         for j in range(cols):
             dfs(0, j, -1, p_set)
-
+            
         for i in range(rows):
             dfs(i, cols-1, -1, a_set)
         for j in range(cols):
             dfs(rows-1, j, -1, a_set)
-
+        
+        
         return list(p_set & a_set)
