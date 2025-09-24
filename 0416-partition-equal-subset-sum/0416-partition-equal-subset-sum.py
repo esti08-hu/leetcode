d78bdbf9bae8168c9497963b61c0d2c641ff9c7f
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-09-24 12:36:16 +0300

    Time: 662 ms (51.57%), Space: 19.9 MB (48.29%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -8,7 +8,7 @@ class Solution:
 
         target = sum(nums) // 2
 
-        for i in range(len(nums)):
+        for i in range(len(nums)-1,-1,-1):
             nextDP = set()
 
             for t in dp:
