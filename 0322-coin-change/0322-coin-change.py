399cc4019382a9437975b9504599db6f0cec08e5
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-09-25 12:37:05 +0300

    Time: 775 ms (50.17%), Space: 18.2 MB (56.83%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -5,5 +5,6 @@ class Solution:
         print(dp)
         for c in coins:
             for i in range(1, amount+1):
-                dp[i] = min(dp[i], dp[i-c]+1)
+                if i >= c:
+                    dp[i] = min(dp[i], dp[i-c]+1)
         return -1 if dp[-1]==float("inf") else dp[-1]
