d3f1531952a596042deda0cfc07377b57ee74d3b
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-10-02 15:19:36 +0300

    Time: 2 ms (71.07%), Space: 18.1 MB (19.15%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,7 +1,21 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        def dfs(start, end):
-            if start == end:
-                return nums[start]
-            return max(nums[start] - dfs(start + 1, end), nums[end] - dfs(start, end - 1))
-        return dfs(0, len(nums) - 1) >= 0
+        n= len(nums)
+        memo = [[-1]*n for i in range(n)]
+
+        def max_diff(i, j):
+            if i==j:
+                return nums[i]
+            
+            if memo[i][j]!=-1:
+                return memo[i][j]
+            
+            diff1 = nums[i]-max_diff(i+1, j)
+            diff2 = nums[j]-max_diff(i, j-1)
+
+            res = max(diff1, diff2)
+
+            memo[i][j] = res
+            return res
+        
+        return max_diff(0,n-1) >= 0
