2bbeb550b1f9d55288ed1a5fdc6e2afd48fd3af1
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-10-02 09:56:23 +0300

    Time: 43 ms (45.89%), Space: 17.8 MB (41.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,17 +1,17 @@
 class Solution:
     def fib(self, n: int) -> int:
-        def dp(x):
-            if n < 2:
-                return n
+        if n < 2:
+            return n 
+            
+        dp = [0,1]
 
-            dp = [0, 1]
+        i = 2
 
-            i=2
-            while i <= x:
-                tmp = dp[1]
-                dp[1] = dp[0] + dp[1]
-                dp[0] = tmp
-                i+=1
-            return dp[1]
-        return dp(n)
+        while i <= n:
+            tmp = dp[1]
+            dp[1] = dp[0] + dp[1]
+            dp[0] = tmp
+
+            i+=1
         
+        return dp[1]
