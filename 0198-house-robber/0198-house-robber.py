3da611f2036d836bdc8a0fef69f6c5d38a2b9d23
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-10-03 15:09:56 +0300

    Time: 3 ms (7.82%), Space: 18.1 MB (13.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        cache = {}
-
+        
+        @cache
         def dfs(i):
             if i>=len(nums):
                 return 0
             
-            if i in cache:
-                return cache[i]
-            
             rob = nums[i]+dfs(i+2)
             skip = dfs(i+1)
 
-            cache[i] = max(rob, skip)
-            return cache[i]
+            return max(rob, skip)
         
         return dfs(0)
