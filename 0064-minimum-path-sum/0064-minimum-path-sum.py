ff23cbc8bc7c40f7f4c041c50f164694028d7209
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-10-05 21:36:36 +0300

    Time: 47 ms (14.01%), Space: 28.5 MB (7.14%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         n,m=len(grid), len(grid[0])
-        
-        cache = {}
 
+        @cache
         def dp(i, j):
             if i==n or j==m:
                 return float("inf")
@@ -11,15 +10,9 @@ class Solution:
             if i==n-1 and j==m-1:
                 return grid[i][j]
 
-            if (i, j) in cache:
-                return cache[(i, j)]
-
             right = grid[i][j] + dp(i, j+1)
             down = grid[i][j] + dp(i+1, j)
 
-            cache[(i, j)] = min(down, right)
-
-            return cache[(i, j)]
+            return min(down, right)
         
         return dp(0,0)
-
