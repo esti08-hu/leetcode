9b7dd9b59025a92aac5e3c13eb6ca603ced3a56a
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-10-10 21:44:00 +0300

    Time: 0 ms (100%), Space: 17.7 MB (59.05%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,14 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        @cache
-        def dfs(i, end):
-            if i>= end:
-                return 0
+        if len(nums) <= 2:
+            return max(nums)
             
-            rob = nums[i] + dfs(i+2, end)
-            skip = dfs(i+1, end)
-        
-            return max(rob, skip)
-        return max(dfs(0, len(nums)-1), dfs(1, len(nums)))
+        def dp1(nums):
+            dp = [nums[-2],nums[-1]]
+            for i in range(len(nums)-3, -1, -1):
+                tmp = dp[0]
+                if dp[0]-nums[i+1] > dp[1]:
+                    dp[0] = dp[0]-nums[i+1]+nums[i]
+                else:
+                    dp[0] = dp[1] + nums[i]
+                dp[1]=tmp
+            
+            return max(dp)
+        print(dp1(nums[:-1]), dp1(nums[1:]))
+        return max(dp1(nums[:-1]), dp1(nums[1:]))
+         
+
