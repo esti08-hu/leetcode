3514f43d597eb38c8a541472f08ed2079b65c62d
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-10-10 16:36:12 +0300

    Time: 0 ms (100%), Space: 17.9 MB (30.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,13 +3,13 @@ class Solution:
         @cache
 
         def dp(i):
-            if i > n:
+            if i < 0:
                 return 0
-            if i == n:
+            if i == 0:
                 return 1
             
-            s1 = dp(i+1)
-            s2 = dp(i+2)
+            s1 = dp(i-1)
+            s2 = dp(i-2)
             return s1+s2
         
-        return dp(0)
+        return dp(n)
