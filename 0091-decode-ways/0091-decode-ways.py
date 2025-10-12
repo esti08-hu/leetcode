d7bbbf398579a7aebb8654e58f75caf2f2a02505
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-10-12 21:58:14 +0300

    Time: 0 ms (100%), Space: 18 MB (30.74%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,26 +1,19 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if s[0] == "0":
+        if not s or s[0] == "0":
             return 0
         
-        cache = {}
+        dp = [0] * (len(s)+1)
+        dp[0] = 1
+        dp[1] = 1
 
-        def dfs(i):
-            if i == len(s):
-                return 1
-
-            if s[i] == "0":
-                return 0
+        for i in range(2, len(s)+1):
+            if s[i-1] != "0":
+                dp[i] += dp[i-1]
             
-            if i in cache:
-                return cache[i]
-            
-            ways = dfs(i+1)
-            if i+1 < len(s) and int(s[i:i+2]) <= 26:
-                ways+=dfs(i+2)
+            two_dig = int(s[i-2:i])
 
-            cache[i] = ways
+            if 10 <= two_dig <= 26:
+                dp[i] += dp[i-2]
 
-            return cache[i]
-        return dfs(0)
-        
+        return dp[len(s)]
