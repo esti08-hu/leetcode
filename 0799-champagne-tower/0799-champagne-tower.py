3a3c8d5f7226c46e4024b99953d983366f61b66f
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-10-29 08:40:43 +0300

    Time: 105 ms (32.23%), Space: 19.9 MB (6.06%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,7 +1,7 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
         cache = {}
-        def dfs(poured, i, j):
+        def dfs(i, j):
             if i < 0 or j < 0 or j > i:
                 return 0.0
             if i == 0 and j == 0:
@@ -9,11 +9,11 @@ class Solution:
             if (i, j) in cache:
                 return cache[(i, j)]
             
-            left = max(0, (dfs(poured, i-1, j-1)-1)/2.0)
-            right = max(0, (dfs(poured, i-1, j)-1)/2.0)
+            left = max(0, (dfs(i-1, j-1)-1)/2.0)
+            right = max(0, (dfs(i-1, j)-1)/2.0)
 
             cache[(i, j)] = left+right
             return cache[(i, j)]
         
-        return min(1, dfs(poured, query_row, query_glass))
+        return min(1, dfs(query_row, query_glass))
 
