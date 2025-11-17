749ec836d0e7064c1ec25eed63690198a85d405c
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-11-17 10:52:23 +0300

    Time: 2590 ms (5%), Space: 20 MB (5.66%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,30 +1,66 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         rows, cols = len(heights), len(heights[0])
-        
-        p_set = set()
+        res = []
+
         a_set = set()
-        
-        def dfs(i, j, prev, visited):
-            if i <0  or j<0  or i>=rows or j>=cols or (i, j) in visited or heights[i][j] < prev:
-                return
-            
-            visited.add((i, j))
-            
-            for di, dj in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                ni, nj = i+di, j+dj
-                
-                dfs(ni, nj, heights[i][j], visited)
-            
-        for i in range(rows):
-            dfs(i, 0, -1, p_set)
-        for j in range(cols):
-            dfs(0, j, -1, p_set)
-            
-        for i in range(rows):
-            dfs(i, cols-1, -1, a_set)
-        for j in range(cols):
-            dfs(rows-1, j, -1, a_set)
-        
-        
-        return list(p_set & a_set)
+        p_set = set()
+        def toPac(r, c, visited):
+            if (r == 0 or c == 0 or 
+                (r, c) in p_set):
+                return 1
+
+            if (r < 0 or r == rows or 
+                c < 0 or c == cols or 
+                (r, c) in visited):
+                return 0
+
+            visited.add((r, c))
+
+            res = 0
+            directions = [[1,0],[-1,0],[0,1],[0,-1]]
+            for dr, dc in directions:
+                nr, nc = r+dr, c+dc
+                if (nr in range(rows) and nc in range(cols) and heights[r][c] >= heights[nr][nc]):
+                    res += toPac(nr, nc, visited)
+                    if res:
+                        return res
+            return res
+
+        def toAtl(r, c, visited):
+            if (r == rows-1 or c == cols-1 or 
+                (r, c) in a_set):
+                return 1
+
+            if (r < 0 or r == rows or 
+                c < 0  or c == cols or 
+                (r, c) in visited):
+                return 0
+
+            visited.add((r, c))
+
+            res = 0
+            directions = [[1,0],[-1,0],[0,1],[0,-1]]
+            for dr, dc in directions:
+                nr, nc = r+dr, c+dc
+                if (nr in range(rows) and nc in range(cols) and heights[r][c] >= heights[nr][nc]):
+                        res += toAtl(nr, nc, visited)
+                        if res:
+                            return res
+            return res
+
+        for r in range(rows):
+            for c in range(cols):
+                pac = toPac(r, c, set())
+                atl = toAtl(r, c, set())
+
+                if pac and atl:
+                    a_set.add((r, c))
+                    p_set.add((r, c))
+                    res.append([r, c])
+                elif pac:
+                    p_set.add((r, c))
+                elif atl:
+                    a_set.add((r, c))
+
+        return res
