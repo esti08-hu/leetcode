0874217b9e05a8e4161834a28efb5895d0e02c88
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-11-19 20:35:56 +0300

    Time: 0 ms (100%), Space: 18 MB (23.51%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -2,6 +2,7 @@ class Solution:
     def countGoodNumbers(self, n: int) -> int:
         MOD = 10**9 + 7
         p = n // 2
+        
         def mod_pow(a, b, mod):
             if b == 0:
                 return 1
@@ -10,7 +11,7 @@ class Solution:
                 return (half * half) % mod
             else:
                 return (half * half * a) % mod
-                
+
         if n % 2:
             return mod_pow(5, p + 1, MOD) * mod_pow(4, p, MOD) % MOD
-        return mod_pow(5, p, MOD) * mod_pow(4, p, MOD) % MOD
+        return mod_pow(5, p, MOD) * mod_pow(4, p, MOD) % MOD 
