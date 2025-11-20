9c9216e7da0e5885a25972549f81ffffb9c9c650
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-11-20 15:22:15 +0300

    Time: 0 ms (100%), Space: 18 MB (22.43%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,25 +1,28 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        phone_dict = defaultdict(str)
+        phone = defaultdict(str)
         k = 0
         for i in range(2, 10):
             for j in range(3):
-                phone_dict[str(i)]+=(chr(k+ord('a')))
+                phone[str(i)]+=(chr(k+ord('a')))
                 k+=1
             if i == 7 or i == 9:
-                phone_dict[str(i)]+=(chr(k+ord('a')))
+                phone[str(i)]+=(chr(k+ord('a')))
                 k+=1
-        combination = [""]    
 
-        for digit in digits:
-            new_comb = []
-            for com in combination:
-                for letter in phone_dict[digit]:
-                    new_comb.append(com+letter)
+        res = []
+        def backtrack(index, current_combination):
+            if index == len(digits):
+                res.append(current_combination)
+                return
+
+            letters = phone[digits[index]]
+
+            for letter in letters:
+                backtrack(index+1, current_combination+letter)
             
-                combination = new_comb
-        
-        return combination
+        backtrack(0, "")
+        return res
         
 
         
