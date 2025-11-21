0e8950af0bf58e44163af0975fe7f3c5ef0fec71
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-11-21 07:48:53 +0300

    Time: 0 ms (100%), Space: 17.8 MB (67.9%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -3,18 +3,22 @@ class Solution:
         phone = {'2': "abc", '3': "def", '4': "ghi", '5': "jkl",
                  '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"}
 
-        combination = [""]
+        combination = []
 
-        for digit in digits:
-            new_combination = []
-            for comb in combination:
-                letters = phone[digit]
-                for letter in letters:
-                    new_combination.append(comb+letter)
+        def backtrack(index, current_combination):
+            if index == len(digits):
+                combination.append(current_combination)
+                return 
             
-            combination = new_combination
-        
+            letters = phone[digits[index]]
+
+            for letter in letters:
+                backtrack(index + 1, current_combination+letter)
+            
+        backtrack(0, "")
         return combination
+
+        
 
 
 
