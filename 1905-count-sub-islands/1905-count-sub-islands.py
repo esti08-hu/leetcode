72e85e3f4529ae214c87b559652638cab7ddf9fc
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-11-28 08:47:02 +0300

    Time: 664 ms (22.27%), Space: 63 MB (13.43%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -2,38 +2,27 @@ class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         rows, cols = len(grid1), len(grid1[0])
 
-        visited = set()
-        def bfs(r, c):
-
-            q = deque()
-            q.append((r, c))
-            visited.add((r, c))
-
-            while q:
-                for i in range(len(q)):
-                    row, col = q.popleft()
-                    directions = [[1,0],[-1,0],[0,1],[0,-1]]
-                    for dr, dc in directions:
-                        nr, nc = row+dr, col+dc
-                        if (nr in range(rows) and 
-                            nc in range(cols)):
-
-                            if ((nr, nc) not in visited and 
-                                grid1[nr][nc] and grid2[nr][nc]):
-                                q.append((nr, nc))
-                                visited.add((nr, nc))
-                            
-                            if self.flag and grid2[nr][nc] and not grid1[nr][nc]:
-                                self.flag = False
-
-        sub_islands = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid1[i][j] and grid2[i][j] and (i, j) not in visited:
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r == rows or c == cols or 
+                not grid2[r][c]):
+                return 
+            if grid2[r][c] and not grid1[r][c]:
+                self.flag = False
+            grid2[r][c] = 0
+            
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for dr, dc in directions:
+                dfs(r+dr, c+dc)
+        
+        sub_island = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid1[r][c] and grid2[r][c]:
                     self.flag = True
-                    bfs(i, j)
-
+                    dfs(r, c)
+                    print(self.flag, (r, c))
                     if self.flag:
-                        sub_islands += 1
-                    
-        return sub_islands
+                        sub_island+=1
+        
+        return sub_island
+
