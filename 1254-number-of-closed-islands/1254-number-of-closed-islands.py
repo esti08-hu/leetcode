556d20674c6877e2542f74c3df68f43a3380abc5
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-11-28 10:14:51 +0300

    Time: 12 ms (83.81%), Space: 18.3 MB (73.14%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,48 +1,28 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        
         rows, cols = len(grid), len(grid[0])
-
-        def bfs(r, c):
-            q = deque()
-            q.append((r, c))
+        
+        def dfs(r, c):
+            if r < 0 or c < 0 or r == rows or c == cols:
+                return False
+            
+            if grid[r][c] != 0:
+                return True
+            
             grid[r][c] = 1
-
-            while q:
-                for i in range(len(q)):
-                    row, col = q.popleft()
-
-                    directions = [[1,0], [-1,0], [0,1], [0,-1]]
-                    for dr, dc in directions:
-                        nr, nc = row+dr, col+dc
-                        if (nr in range(rows) and 
-                            nc in range(cols) and 
-                            not grid[nr][nc]):
-                            q.append((nr, nc))
-                            grid[nr][nc] = 1
-
-        for r in range(rows):
-            if not grid[r][0]:
-                bfs(r, 0)
-
-        for r in range(rows):
-            if not grid[r][cols-1]:
-                bfs(r, cols-1)
-
-        for c in range(cols):
-            if not grid[0][c]:
-                bfs(0, c)
-
-        for c in range(cols):
-            if not grid[rows-1][c]:
-                bfs(rows-1, c)
+            
+            down = dfs(r+1, c) 
+            up = dfs(r-1, c) 
+            right = dfs(r, c+1) 
+            left = dfs(r, c-1)
+            
+            return right and left and up and down
         
-        closed_island = 0
-
-        for r in range(1, rows-1):
-            for c in range(1, cols-1):
-                if not grid[r][c]:
-                    bfs(r, c)
-                    closed_island += 1
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0:
+                    if dfs(r, c):
+                        count += 1
         
-        return closed_island
+        return count
