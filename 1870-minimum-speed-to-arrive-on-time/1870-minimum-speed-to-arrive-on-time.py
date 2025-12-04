5fae3b645afe1a409394bb45324005e47a756889
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-12-04 17:32:00 +0300

    Time: 2062 ms (14.24%), Space: 28.5 MB (94.28%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,50 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        '''
+        0 1 2 
+        1 3 2 
+        mx = 3
+        mi = 0
+        1 + 3 + 2 => 6
+        mid = 1
+
+        hour = 6
+dist = [1,3,2,4], hour = 6
+
+mx = 4
+mi = 0
+mid = 1
+1 + 1 + 0.7 = 2.7 == 2.7 =>  
+ans = -1
+if i == len(nums) - 1:
+    curr_hour += dist[i]/mid
+else:
+    curr_hour += math.ceil(dist[i]/mid)
+
+if curr_hour == hour => return mid
+if curr_hour > hour  =>  mi = mid + 1
+if curr_hour < hour => ans = mid; mx = mid
+
+return ans
+
+        '''
+        mx = 10**7 + 1
+        mn = 1
+
+        ans = -1
+        while mn < mx:
+            mid = (mx + mn)//2
+            curr_hour = 0
+            for i in range(len(dist)):
+                if i == len(dist)-1:
+                    curr_hour+=dist[i]/mid
+                else:
+                    curr_hour+=math.ceil(dist[i]/mid)
+
+            if curr_hour <= hour:
+                mx = mid
+
+            elif curr_hour*100 > hour*100:
+                mn = mid + 1
+
+        return mn if mn < 10**7+1 else -1
