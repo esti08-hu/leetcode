8caf96e961c57ae65fcc689642d4a6c286bcd8e2
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-12-07 14:29:50 +0300

    Time: 0 ms (100%), Space: 17.9 MB (45.52%) - LeetHub

---
+++ 1201-ugly-number-iii/1201-ugly-number-iii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def nthUglyNumber(self, n: int, a: int, b: int, c: int) -> int:
+        def check(mid):
+            total = mid//a + mid//b + mid//c - mid//ab - mid//ac - mid//bc + mid//abc
+            return total >= n
+            
+        def lcm(x, y):
+            return x * y // math.gcd(x, y)
+        
+        ab = lcm(a, b)
+        ac = lcm(a, c)
+        bc = lcm(b, c)
+        abc = lcm(a, bc)
+
+        l, r = 1, 2 * 10**9
+        while l < r:
+            mid = (l + r) // 2
+            if check(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
+
+
