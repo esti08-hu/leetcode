113edafba013aa3a0eb1c1beae189f2c8e197245
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-12-15 16:18:33 +0300

    Time: 3 ms (76.83%), Space: 19 MB (70.76%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,28 +1,36 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        inDegree = [0]*numCourses
+        '''
+        build the graph and indegree
+        enqueue courses indegeree with 0
+        process or add the res 
+        '''
+
+        graph = [[] for i in range(numCourses)]
+        indegree = [0] * numCourses
 
         for a, b in prerequisites:
-            inDegree[a] +=1
-        
-
-        graph = {i:[] for i in range(numCourses)}
-
-        for a, b in prerequisites:
+            indegree[a]+=1
             graph[b].append(a)
-
+        
+        res = []
         q = deque()
         for i in range(numCourses):
-            if inDegree[i] == 0:
+            if indegree[i] == 0:
+                res.append(i)
                 q.append(i)
-        order = []
-        while q:
-            course = q.popleft()
-            order.append(course)
-
-            for nb in graph[course]:
-                inDegree[nb] -= 1
-                if inDegree[nb] == 0:
-                    q.append(nb)
         
-        return order if len(order)==numCourses else []
+        while q:
+            for i in range(len(q)):
+                course = q.popleft()
+
+                for nei in graph[course]:
+                    indegree[nei] -= 1
+                    if indegree[nei] == 0:
+                        q.append(nei)
+                        res.append(nei)
+        
+        return res if len(res) == numCourses else []
+        
+
+
