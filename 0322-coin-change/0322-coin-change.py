5f958f7112ac7bd02b6b13a7b89804464d06e98b
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-12-18 10:04:53 +0300

    Time: 946 ms (28.57%), Space: 38.8 MB (15.71%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,23 +1,16 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        INF = float('inf')
-        cache = {}
-        
-        def dp(a):
+        @cache
+        def dfs(a):
+            if a < 0:
+                return float("inf")
             if a == 0:
                 return 0
-            if a < 0:
-                return INF
-            if a in cache:
-                return cache[a]
             
-            min_coins = INF
-            for c in coins:
-                res = dp(a - c)
-                if res != INF:
-                    min_coins = min(min_coins, res + 1)
-            cache[a] = min_coins
-            return min_coins
-
-        ans = dp(amount)
-        return -1 if ans == INF else ans
+            curr_min = float("inf")
+            for coin in coins:
+                curr_min = min(curr_min, 1 + dfs(a - coin))
+            
+            return curr_min
+        res = dfs(amount)
+        return res if res != float("inf") else -1
