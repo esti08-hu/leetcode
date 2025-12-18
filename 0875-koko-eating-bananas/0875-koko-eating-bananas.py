931658378fa6b095c439326923b1eb3b30b7547a
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-12-18 09:40:28 +0300

    Time: 157 ms (54.88%), Space: 19.1 MB (37.93%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,36 +1,26 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # if len(piles) > h:
-        #     return 
         '''
-        mn = 1
-        mx = max(piles)
-
-        check(mid)
-            curr_hour = 0
-            for i in range(len(piles))
-                curr_hour += math.ceil(piles[i]/mid)
-                if curr_hour > h:
-                    return False
-            
-            return True
-            
+        0 1 2 3 4 5 6 7 8 9 10 11
         '''
-        def check(mid):
-            curr_hour = 0
-            for i in range(len(piles)):
-                curr_hour += math.ceil(piles[i]/mid)
-                if curr_hour > h:
-                    return False
-            return True
+        if h < len(piles):
+            return -1
 
-        mn, mx = 1, max(piles)
+        def can_finish(min_speed):
+            hours = 0
 
-        while mn <= mx:
-            mid = (mn + mx) // 2
-            if check(mid):
-                mx = mid - 1
+            for pile in piles:
+                hours+=math.ceil(pile/min_speed)
+
+            return hours <= h
+
+        low, high = 1, max(piles)
+        while low < high:
+            mid = (low + high) // 2
+
+            if can_finish(mid):
+                ans = mid
+                high = mid
             else:
-                mn = mid + 1
-        
-        return mn
+                low = mid + 1
+        return low
