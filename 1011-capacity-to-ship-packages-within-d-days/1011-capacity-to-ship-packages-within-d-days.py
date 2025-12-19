33b95831ecf98e4cbbb63236a420f26f9c4af9cf
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-12-19 08:33:31 +0300

    Time: 136 ms (96.77%), Space: 21.9 MB (98.22%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,31 +1,55 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
+        '''
+        low = days equal to len weights
+        high = their is only one day
+
+        low = max(weights)
+        high = sum(weights)
+        mid = (low + high) >> 1
+        can_ship(max_cap):
+
+        curr_sum = 0
+        count = 1
+
+        for weight in weights:
+            curr_sum += weight
+
+            if curr_sum > max_cap:
+                count += 1
+                curr_sum = weight
+        return count <= days
+
+        while low < right:
+            mid = (low + high) // 2
+            if can_ship(mid):
+                high = mid
+            else:
+                left = mid + 1
+        return low 
+
+        '''
+        def can_ship(max_cap):
+            curr_sum = 0
+            count = 1
+            
+            for weight in weights:
+                curr_sum += weight
+
+                if curr_sum > max_cap:
+                    count += 1
+                    curr_sum = weight
+                
+            return count <= days
+
         low, high = max(weights), sum(weights)
 
-        def check(cap):
-            w_sum = 0
-            D = 0
-            for w in weights:
-                w_sum += w
-                if w_sum == cap:
-                    D+=1
-                    w_sum = 0
-                elif w_sum > cap:
-                    D+=1
-                    w_sum=0
-                    w_sum += w
-            if w_sum > 0:
-                D+=1
-            return D <= days
+        while low < high:
+            mid = (low + high) >> 1
 
-
-
-        while low <= high:
-            mid = (low + high) // 2
-
-            if check(mid):
-                high = mid-1
+            if can_ship(mid):
+                high = mid
             else:
                 low = mid + 1
         
-        return low 
+        return low
