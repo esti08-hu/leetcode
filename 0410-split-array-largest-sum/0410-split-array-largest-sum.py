6717e31d65d778efcb8c7a29c8acebd786d2fd99
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-12-22 08:03:56 +0300

    Time: 1 ms (77.97%), Space: 17.2 MB (99.92%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,28 +1,25 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        def can_split(max_capacity):
+        def can_split(max_cap):
             cuts = 1
-            current_sum = 0
+            curr_sum = 0
 
             for num in nums:
-                current_sum += num
-                
-                if current_sum > max_capacity:
-                    cuts+=1
-                    current_sum = num
+                curr_sum += num
+                if curr_sum > max_cap:
+                    cuts += 1
+                    curr_sum = num
             
             return cuts <= k
         
-
-        low = max(nums)
-        high = sum(nums)
+        low, high = max(nums), sum(nums)
 
         while low < high:
-            mid  = (low + high) // 2
+            mid = (low + high) >> 1
 
             if can_split(mid):
                 high = mid
             else:
                 low = mid + 1
-        
+            
         return low
