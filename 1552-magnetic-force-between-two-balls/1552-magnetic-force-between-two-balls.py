d459c17c0c406f7df744aa87d2435a73ad28540b
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-12-26 19:31:22 +0300

    Time: 388 ms (73.78%), Space: 29.4 MB (95.68%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
@@ -1,29 +1,45 @@
 class Solution:
     def maxDistance(self, position: List[int], m: int) -> int:
+        '''
+        0 1 2 3 4
+        5,4,3,2,1
+
+        1,2,3,4,7
+        last_p = position[0]
+        count = 1
+        for p in position:
+            gap = p - last_p
+            if gap >= mid:
+                last_p = p
+                count += 1
+
+        return count >= m
+
+        low = 1
+        high = max(position)
+        '''
         position.sort()
 
-        def check(force):
+        def can(mid):
+            last_p = position[0]
             count = 1
-            last_pos = position[0]
-            for i in range(1, len(position)):
-                if position[i]-last_pos >= force:
-                    count+=1
-                    last_pos = position[i]
-                    if count == m:
-                        return True
-            return False
+            for p in position:
+                gap = p - last_p
+                if gap >= mid:
+                    last_p = p
+                    count += 1
+            
+            return count >= m
         
-        left, right = 1, position[-1]-position[0]
-        res = 0
+        low, high = 1, max(position)
+        ans = -1
+        while low <= high:
+            mid = (low + high) // 2
 
-        while left <= right:
-
-            mid = (left+right)//2
-
-            if check(mid):
-                res = mid
-                left = mid + 1
+            if can(mid):
+                ans = mid
+                low = mid + 1
             else:
-                right = mid - 1 
+                high = mid - 1
         
-        return res
+        return ans
