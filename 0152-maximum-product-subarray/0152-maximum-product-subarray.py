20e30f4a0714fc9e49ed6d818ea51929db807064
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-12-27 08:57:55 +0300

    Time: 11 ms (53.74%), Space: 18.1 MB (89.77%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,38 +1,59 @@
-from typing import List
-
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if not nums:
-            return 0 
-        
-        n = len(nums)
-        max_cache = {}
-        min_cache = {}
-        
-        def max_dp(i):
-            if i == 0:
-                return nums[0]
-            if i in max_cache:
-                return max_cache[i]
+        '''
 
-            prev_max = max_dp(i-1)
-            prev_min = min_dp(i-1)
-            max_cache[i] = max(nums[i], nums[i] * prev_max, nums[i] * prev_min)
-            return max_cache[i]
-        
-        def min_dp(i):
-            if i == 0:
-                return nums[0]
-            if i in min_cache:
-                return min_cache[i]
+        max_prod = 2
+        a = 2
+        b = -18
 
-            prev_max = max_dp(i-1)
-            prev_min = min_dp(i-1)
-            min_cache[i] = min(nums[i], nums[i] * prev_max, nums[i] * prev_min)
-            return min_cache[i]
-        
-        result = float("-inf")
-        for i in range(n):
-            result = max(result, max_dp(i))
-        
-        return result
+        if a <= 0 or n <= 0:
+            a = n
+        elif a > 0:
+            a *= n
+
+        if b == 0:
+            b = n
+        else:
+            b *= n
+
+        max_prod = max(a, b, max_prod)
+
+        [-2,3,3,-2,-1,-1]
+        [-2,3,-1]
+        [0,0,0,1,2]
+
+        [2,-5,-2,-4,3]
+         2 2  20 20 24
+        '''
+        a,b,max_prod = nums[0],nums[0],nums[0]
+
+        for n in nums[1:]:
+            if a <= 0 or n <= 0:
+                a = n
+            else:
+                a *= n
+
+            if b == 0:
+                b = n
+            else:
+                b *= n
+
+            max_prod = max(max_prod, a, b)
+
+        nums = nums[::-1]
+
+        a,b = nums[0],nums[0]
+        for n in nums[1:]:
+            if a <= 0 or n <= 0:
+                a = n
+            else:
+                a *= n
+
+            if b == 0:
+                b = n
+            else:
+                b *= n
+            
+            max_prod = max(max_prod, a, b)
+
+        return max_prod
