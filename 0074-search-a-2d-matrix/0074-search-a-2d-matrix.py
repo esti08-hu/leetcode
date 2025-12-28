ec10b994c747b9f48c0d0fc7cb7e78fa05502bd0
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2025-12-28 08:44:08 +0300

    Time: 0 ms (100%), Space: 17.7 MB (91.1%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,24 +1,46 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        """
-        row = 0 col = 3
-        if (0, 3) < target:
-            row += 1
-        if (row, col) > taret:
-            row-=1
-        else:
-            return True
-        """
+        '''
+        m = 2
+        n = 3
+        low = [0, 0]
+        high = [2, 3]
 
-        row, col = len(matrix), len(matrix[0])
-        r, c = 0, col-1
+        mid = [1, 1]
+        00 - 34
+        1+3//2
+        4+1//2
+        22
+        high = (pr, pc-1)
 
-        while r < row and c >= 0:
-            if matrix[r][c] < target:
-                r+=1
-            elif matrix[r][c] > target:
-                c-=1
-            else:
+        00 - 21
+        11
+
+        total = 12
+        6 
+        6//cols = cur_row
+        6%cols = cur_col
+
+        22
+        2*cols + cur_col = 6
+
+        '''
+
+        rows, cols = len(matrix), len(matrix[0])
+
+        total = rows * cols
+        high = total - 1
+        low = 0
+        while low <= high:
+            mid = (low + high) // 2
+            cur_row = mid//cols
+            cur_col = mid%cols
+
+            if matrix[cur_row][cur_col] == target:
                 return True
-        
+            elif matrix[cur_row][cur_col] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+
         return False
