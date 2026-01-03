a9e3232f79e1808422da855586d9388af83c5ecc
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2026-01-03 08:53:13 +0300

    Time: 8 ms (37.12%), Space: 18.9 MB (28.17%) - LeetHub

---
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minMoves(self, nums: List[int]) -> int:
+        min_num = min(nums)
+        res = 0
+        for num in nums:
+            res += abs(num - min_num)
+        
+        return res
