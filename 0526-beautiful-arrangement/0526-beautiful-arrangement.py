8ce9deee418b8b9851bf0b45a6582ac7b4a6838a
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2026-01-14 08:15:20 +0300

    Time: 1323 ms (5.1%), Space: 19.5 MB (23.82%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -2,13 +2,15 @@ class Solution:
     def countArrangement(self, n: int) -> int:
         self.count = 0
         self.seen = set()
+        # @cache
         def dfs(perm):
             if len(perm) == n:
                 self.count += 1
                 return 
             
             for i in range(1, n+1):
-                if (not perm or (not ((len(perm)+1)%(i)) or not ((i)%(len(perm)+1)))) and i not in self.seen :
+                pos = len(perm) + 1
+                if (not perm or (pos % i == 0 or i % pos == 0)) and i not in self.seen:               
                     perm.append(i)
                     self.seen.add(i)
                     dfs(perm)
