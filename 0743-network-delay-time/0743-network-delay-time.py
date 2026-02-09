e3a9308321e11e68aa3da9a35b3db29d2f25092f
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2026-02-09 10:22:29 +0300

    Time: 314 ms (99.63%), Space: 21.4 MB (21.1%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,36 +1,26 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        '''
-        node_list = [1 1 1 1]
-        {2: [[1, 1], [3, 1]], 3: [[4, 1]]}
-        time = 0 + 1 + 1 + 1 = 3
-        q = []
-        node = 4
-        node_list[node-1] = 1
-        check if node in dictioary
+        graph = [[] for _ in range(n)]
 
-
-        '''
-        graph = defaultdict(list)
-        for time in times:
-            u, v, w = time
-            graph[u].append([v, w])
-        heap = [(0, k)]
-        distances = {}
-
-        while heap:
-            time, node = heapq.heappop(heap)
-
-            if node in distances:
-                continue
+        for u, v, w in times:
+            graph[u-1].append((v-1, w))
             
-            distances[node] = time
+        min_heap = [(0, k-1)]
+        dist = [float("inf")] * n
+        dist[k-1] = 0
+        
+        seen = set()
 
-            for v, w in graph[node]:
-                if v not in distances:
-                    heapq.heappush(heap, (time + w, v))
+        while min_heap:
+            curr_w, node = heapq.heappop(min_heap)
+            if node in seen:
+                continue
+            seen.add(node)
+            for nei, w in graph[node]:
+                new_w = curr_w + w
+                if dist[nei] > new_w:
+                    heapq.heappush(min_heap, (new_w, nei))
+                    dist[nei] = new_w
         
-        if len(distances) != n:
-            return -1
-        
-        return max(distances.values())
+        res = max(dist)
+        return res if res != float("inf") else -1
