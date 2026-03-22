63d7a7ac328fd7f8d2231877f1aa4d59674068db
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2026-03-22 07:20:32 +0300

    Time: 246 ms (53.84%), Space: 21.6 MB (64.78%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,29 +2,25 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
 
-        def bfs(r, c):
-            q = deque()
-            q.append((r, c))
+        dir = [(0,1), (0,-1), (1,0), (-1,0)]
+        def dfs(r, c):
+            if r < 0 or c < 0 or r == rows or c == cols or grid[r][c] == "0":
+                return
+            
             grid[r][c] = "0"
 
-            while q:
-                for i in range(len(q)):
-                    row, col = q.popleft()
-
-                    directions = [(0,1), (0,-1), (1,0), (-1,0)]
-                    for dr,dc in directions:
-                        nr, nc = row + dr, col + dc
-
-                        if nr in range(rows) and nc in range(cols) and grid[nr][nc] == "1":
-                            grid[nr][nc] = "0"
-                            q.append((nr, nc))
+            for dr, dc in dir:
+                nr, nc = r+dr, c+dc
+                dfs(nr, nc)
         
         islands = 0
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
-                    bfs(r, c)
+                    dfs(r, c)
+                
                     islands+=1
         
         return islands
+        
