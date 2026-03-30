9a56a9145bfd57160c2cc82e9a92fb257fdf3d46
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2026-03-30 07:06:07 +0300

    Time: 33 ms (66.71%), Space: 31.3 MB (96.92%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,35 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        '''
-        max_sum = 6
-        curr = 5
-
-        if curr + nums[i] > max_sum:
-            max_sum = max(curr+nums[i], nums[i])
-            curr = max(curr+nums[i], nums[i])
-    
-        elif curr + nums[i] < 0:
-            curr = max(0, nums[i])
-
-        else:
-            curr += nums[i]
-
-        '''
-
         max_sum = nums[0]
-        curr = nums[0]
+        curr_sum = nums[0]
 
-        for i in range(1, len(nums)):
-            if curr + nums[i] > max_sum:
-                max_sum = max(curr+nums[i], nums[i])
-                curr = max(curr+nums[i], nums[i])
-            elif curr + nums[i] < 0:
-                if curr < 0:
-                    curr = nums[i]
-                    max_sum = max(max_sum, curr)
-                else:
-                    curr = max(0, nums[i])
-            else:
-                curr += nums[i]
-
-        return max(curr, max_sum)
+        for num in nums[1:]:
+            curr_sum = max(curr_sum, 0)
+            curr_sum += num
+            max_sum = max(max_sum, curr_sum)
+        
+        return max_sum
