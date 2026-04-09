f5afe3f7a21ce19953eaa343290508ebd50c4452
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2026-04-09 16:45:59 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -2,12 +2,17 @@ class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         res = []
         sorted_nums = sorted(nums)
+
+        hash_map = {sorted_nums[0]: 0}
+        x = 0
+
+        for i in range(1, len(nums)):
+            if sorted_nums[i] not in hash_map:
+                x += 1
+                hash_map[sorted_nums[i]] = x
+            else:
+                x += 1
         for i in range(len(nums)):
-            curr = 0
-            for j in range(len(nums)):
-                if nums[i] <= sorted_nums[j]:
-                    res.append(curr)
-                    break
-                curr += 1
-        
+            res.append(hash_map[nums[i]])
+
         return res
