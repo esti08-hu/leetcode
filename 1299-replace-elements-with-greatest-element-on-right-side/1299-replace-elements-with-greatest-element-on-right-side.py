51155d4c3b6f7ce693d2f6e9dc69eef3e56b3450
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2026-05-13 08:00:04 +0300

    Time: 27 ms (37.67%), Space: 21.3 MB (8.03%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -3,7 +3,6 @@ class Solution:
         res = [-1]
         greatest = arr[-1]
         for i in range(len(arr) - 2, -1, -1):
-            print(arr[i])
             res.append(greatest)
             greatest = max(greatest, arr[i])
         
