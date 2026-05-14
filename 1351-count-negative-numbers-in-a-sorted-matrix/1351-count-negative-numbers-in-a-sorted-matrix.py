02d555695b6002ec45375d07fdb8d12992c3722e
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2026-05-14 08:28:14 +0300

    Time: 3 ms (26.63%), Space: 20.4 MB (13.82%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -4,8 +4,10 @@ class Solution:
         cols = len(grid[0])
 
         count = 0
-        for r in range(rows):
-            for c in range(cols):
+        for r in range(rows-1, -1, -1):
+            for c in range(cols-1, -1, -1):
                 if grid[r][c] < 0:
                     count += 1
+                elif grid[r][c] >= 0:
+                    break
         return count
