e4c8ad0a99f3033dd63294fc073f543bea69f7de
Estifanos A. <98952769+esti08-hu@users.noreply.github.com>
2026-07-08 10:46:13 +0300

    Time: 527 ms (36.28%), Space: 22.8 MB (5.74%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        word_dict = {}
+        for word in words:
+            word_dict[word] = set(word)
+        
+        max_prod = 0
+
+        for i in range(len(words)-1):
+            curr = word_dict[words[i]]
+            for j in range(i+1, len(words)):
+                for c in curr:
+                    if c in word_dict[words[j]]:
+                        break
+                else:
+                    max_prod = max(max_prod, len(words[i]) * len(words[j]))
+
+        return max_prod
+
+
+        return 0
